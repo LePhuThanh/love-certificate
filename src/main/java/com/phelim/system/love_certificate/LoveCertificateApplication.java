7a070b3df3582988c60f1f3ca19cc30258a1ea4f
LePhuThanh <lpthanh710@gmail.com>
2026-04-10 19:37:36 +0700

    init project

---
+++ src/main/java/com/phelim/system/love_certificate/LoveCertificateApplication.java
@@ -0,0 +1,13 @@
+package com.phelim.system.love_certificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveCertificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveCertificateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phelim/system/love_certificate/LoveCertificateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phelim.system.love_certificate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveCertificateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
