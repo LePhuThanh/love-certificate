2066f5696539db3d6e1f234aaecc7af3440cc372
LePhuThanh <lpthanh710@gmail.com>
2026-04-11 09:48:28 +0700

    update: note workflow & detail api & disable openhtmltopdf log

---
+++ src/main/resources/sql/rule_config.sql
@@ -0,0 +1,3 @@
+INSERT INTO rule_config (min_days, type) VALUES (365, 'A');
+INSERT INTO rule_config (min_days, type) VALUES (730, 'B');
+INSERT INTO rule_config (min_days, type) VALUES (1825, 'C');
